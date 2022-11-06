b0249fbf107bf2b2bba305c28320c52987810b92
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-11-07 01:27:50 +0600

    server setup

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const app = express()
 const cors = require('cors');
 require('dotenv').config();
+const app = express()
 const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('hospitology server')
-
 })
 
 app.listen(port, () => {
     console.log('server running', port)
 })
+
+
+
