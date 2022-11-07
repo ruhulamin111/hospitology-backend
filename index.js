92b7c9449e1744a36fb02493fc6efd6a30ba1ee8
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-11-07 12:21:37 +0600

    database connect

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -15,5 +16,22 @@ app.listen(port, () => {
     console.log('server running', port)
 })
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6e9yqor.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect()
+        console.log('db connected');
+
+        const doctorsCollection = client.db("hospitology").collection("doctors");
+
+
+
+    } finally {
+
+    }
+}
+
+run().catch(console.dir)
 
