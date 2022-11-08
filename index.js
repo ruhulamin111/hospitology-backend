faaa706c5c98b05e2b951f66bab901e100f8b651
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-11-08 12:30:23 +0600

    searh item done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -37,6 +37,13 @@ async function run() {
                 return
             }
             const result = await doctorsCollection.find({ branch: { $regex: branch, $options: 'i' }, department: { $regex: department, $options: 'i' } }).toArray()
+            res.send(result)
+        })
+
+        app.get('/doctors/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await doctorsCollection.findOne(query)
             res.send(result)
         })
 
